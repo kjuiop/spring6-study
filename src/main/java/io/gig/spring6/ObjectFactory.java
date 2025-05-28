235db30b7fa28fe2b6a122a4221e727b4541e7c9
kjuiop <copying_y@naver.com>
2025-05-28 12:50:16 +0900

    feat: spring bean factory apply

--- src/main/java/io/gig/spring6/ObjectFactory.java
+++ src/main/java/io/gig/spring6/ObjectFactory.java
@@ -1,16 +1,23 @@
 package io.gig.spring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author : JAKE
  * @date : 2025/05/28
  */
+// spring bean 등록
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExtRateProvider();
+        return new SimpleExRateProvider();
     }
 }
